b72ce0812bfb496924c2016ce4352a208f45cba0
TasneemDweiri <137085470+TasneemDweiri@users.noreply.github.com>
2026-09-02 00:53:24 +0300

    Refactor PBaseSettings model_config configuration
    Updated model_config to use SettingsConfigDict with new parameters.

--- settings/base.py
+++ settings/base.py
@@ -1,6 +1,9 @@
-from pydantic_settings import BaseSettings
-from pydantic import ConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class PBaseSettings(BaseSettings):
-    model_config = ConfigDict(env_file = './.env', extra="allow")
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
